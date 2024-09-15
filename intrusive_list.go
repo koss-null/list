fb92b60aacd14476d2229a2100f9e0f8ea138ee9
Dima Kossovich <koss@null.net>
2024-09-15 20:52:34 +0300

    add intrusive lists to readme

--- intrusive_list.go
+++ intrusive_list.go
@@ -1,21 +1,24 @@
 package list
 
-// List is a struct to embed in your struct to make it singly linked list
+// List[T nexter[T]] is a type parameterized struct. It should be embedded in other structs
+// to provide them with the ability to be nodes in a singly linked list
 type List[T nexter[T]] struct {
 	next *T
 }
 
-// Next returns the next linked list element
+// Next() returns a pointer to the next element in the linked list
 func (l *List[T]) Next() *T {
 	return l.next
 }
 
-// SetNext sets element 'next' as the next list element
+// SetNext() assigns the 'next' pointer to the provided element, effectively making
+// 'next' the subsequent element in the list
 func (l *List[T]) SetNext(next *T) {
 	l.next = next
 }
 
-// SetNext insert element 'elem' as the next list element, next element becomes elem.next
+// InsertNext() inserts the provided element 'elem' as the subsequent element in the list.
+// The element that was previously next becomes the next element of 'elem'
 func (l *List[T]) InsertNext(elem *T) {
 	if elem == nil {
 		return
@@ -24,7 +27,8 @@ func (l *List[T]) InsertNext(elem *T) {
 	l.next = elem
 }
 
-// RemoveNext removes next element from the list, saves all elements after the next
+// RemoveNext() detaches the next element from the list. The element following the next
+// element (if it exists) becomes the new next element
 func (l *List[T]) RemoveNext() {
 	if l.next != nil {
 		l.next = (*l.next).Next()
@@ -33,8 +37,11 @@ func (l *List[T]) RemoveNext() {
 	l.next = nil
 }
 
-// nexter is an internal interface to fence the T parameter of the node: it should embed List struct
+// nexter[T any] is a type parameterized internal interface used to constrain the T parameter.
+// Structs that embed the List struct should also implement this interface with Next() and SetNext() methods.
 type nexter[T any] interface {
 	Next() *T
 	SetNext(*T)
+	InsertNext(*T)
+	RemoveNext()
 }
